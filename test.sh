f3678fe73a4b54f5c43622b49cdc01d48f16dd4f
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2025-08-23 10:20:35 -0600

    Add schema validation for gallery-video-list.schema.json

--- test.sh
+++ test.sh
@@ -2,3 +2,4 @@
 
 node tests/schema.test.js schemas/alum-info-list.schema.json src/_data/alum-info-list.json
 node tests/schema.test.js schemas/faq-list.schema.json src/_data/faq-list.json
+node tests/schema.test.js schemas/gallery-video-list.schema.json src/_data/gallery-video-list.json

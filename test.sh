0d8e76adfffc9dfafdbb898c6c38be5ff417d469
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2025-08-23 10:09:54 -0600

    Add schema validation for faq-list.schema.json

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 node tests/schema.test.js schemas/alum-info-list.schema.json src/_data/alum-info-list.json
+node tests/schema.test.js schemas/faq-list.schema.json src/_data/faq-list.json

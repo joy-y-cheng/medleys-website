47fee0c6eedb7669d9e652dc2b70df8ca5274bfa
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2025-08-23 21:16:07 -0600

    Add schema validation for member-info-list.schema.json

--- test.sh
+++ test.sh
@@ -3,3 +3,4 @@
 node tests/schema.test.js schemas/alum-info-list.schema.json src/_data/alum-info-list.json
 node tests/schema.test.js schemas/faq-list.schema.json src/_data/faq-list.json
 node tests/schema.test.js schemas/gallery-video-list.schema.json src/_data/gallery-video-list.json
+node tests/schema.test.js schemas/member-info-list.schema.json src/_data/member-info-list.json

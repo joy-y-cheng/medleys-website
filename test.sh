9f6a716e143a4a792fef171dd76a940d932612f4
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2025-08-23 21:18:30 -0600

    Add schema validation for navigation.schema.json

--- test.sh
+++ test.sh
@@ -4,3 +4,4 @@ node tests/schema.test.js schemas/alum-info-list.schema.json src/_data/alum-info
 node tests/schema.test.js schemas/faq-list.schema.json src/_data/faq-list.json
 node tests/schema.test.js schemas/gallery-video-list.schema.json src/_data/gallery-video-list.json
 node tests/schema.test.js schemas/member-info-list.schema.json src/_data/member-info-list.json
+node tests/schema.test.js schemas/navigation.schema.json src/_data/navigation.json

2dec20126ae090ca997e47611cf9eab87a503764
nouturnsign <nouturnsign@users.noreply.github.com>
2025-09-14 00:44:41 +0000

    deploy: 8f491b9512ba28f07bc1048e21e414a881690e76

---
+++ js/gallery.js
@@ -0,0 +1,27 @@
+const carouselDiv = document.getElementById("carousel-performances");
+
+// This is the index of the active player.
+var carouselActiveIndex = 0;
+// This is the array of all players, one for each video.
+var carouselPlayers = [];
+
+// Initialize players.
+function onYouTubeIframeAPIReady() {
+    let iframes = carouselDiv.getElementsByTagName("iframe");
+    for (let iframe of iframes) {
+        let player = new YT.Player(iframe.id);
+        carouselPlayers.push(player);
+    }
+
+    carouselDiv.addEventListener("slid.bs.carousel", (event) => {
+        // On slide (end of slide), pause the current video and advance to previous/next video.
+        carouselPlayers[carouselActiveIndex].pauseVideo();
+        if (event.direction == "right") carouselActiveIndex -= 1;
+        else carouselActiveIndex += 1;
+
+        // Enforce 0 <= carouselActiveIndex < carouselPlayers.length.
+        if (carouselActiveIndex < 0) carouselActiveIndex += carouselPlayers.length;
+        if (carouselActiveIndex >= carouselPlayers.length)
+            carouselActiveIndex -= carouselPlayers.length;
+    });
+}
